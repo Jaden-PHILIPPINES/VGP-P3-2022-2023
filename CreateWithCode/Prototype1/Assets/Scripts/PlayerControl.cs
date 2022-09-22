25f4a654f50b899aeb5726c8d55387530398144c
Jaden Morales <152729@student.vansd.org>
2022-09-22 10:32:46 -0700

    AHHAHAHA I DID IIIIIT
    The final touches to the game, its finally playable!

--- CreateWithCode/Prototype1/Assets/Scripts/PlayerControl.cs
+++ CreateWithCode/Prototype1/Assets/Scripts/PlayerControl.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +19,10 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
-        // We'll move the vehicle
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
