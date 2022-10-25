a1e8c2b5f2dc0a5974479dbfb77cea246d55559f
Jaden Morales <152729@student.vansd.org>
2022-10-25 09:52:38 -0700

    fixes
    Fixed the crash

--- CreateWithCode/Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -19,7 +19,8 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound) 
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound) 
+        } 
+        else if (transform.position.z < lowerBound) 
         { 
             Destroy(gameObject);
         }
--- CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
-    private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
     
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); 
     }
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Update is called once per frame
     void Update()
