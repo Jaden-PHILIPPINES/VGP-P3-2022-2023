a38dd61130b461197f067eb12b5d7ef37581e13b
Jaden Morales <152729@student.vansd.org>
2022-11-29 10:36:45 -0800

    fixed the error
    smooth backround, countered the unjumpable issue,

--- CreateWithCode/Prototype 3/Assets/Course Library/Scripts/RepeatBlackround.cs
+++ CreateWithCode/Prototype 3/Assets/Course Library/Scripts/RepeatBlackround.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RepeatBackround : MonoBehaviour
+public class RepeatBlackround : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position; 
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - 50)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
-            transform.positon = startPos;
+            transform.position = startPos;
         }
     }
 }
