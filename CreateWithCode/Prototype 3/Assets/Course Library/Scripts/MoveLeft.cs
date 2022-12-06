e96fdd137c55d1e3c56bf8cd9f7689eb63c19b6b
Jaden Morales <152729@student.vansd.org>
2022-12-06 10:26:22 -0800

    Revert "Fuck you luck"
    This reverts commit d172736fc5497c08a7d96b9e75599b8c711769b5.

--- CreateWithCode/Prototype 3/Assets/Course Library/Scripts/MoveLeft.cs
+++ CreateWithCode/Prototype 3/Assets/Course Library/Scripts/MoveLeft.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
-    
+    private PlayerController PlayerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PlayerControllerScript.gameover == false)
+        {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
     }
 }
