43abb3cb75c6ecb94b7825209e58a820f5deada4
Jaden Morales <152729@student.vansd.org>
2022-12-09 10:04:20 -0800

    uiyh
    jnbhgrfeNEWENUHGGER

---
+++ CreateWithCode/Prototype3/Assets/Course Library/Scripts/MoveLeft.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController PlayerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
+    }
+}
